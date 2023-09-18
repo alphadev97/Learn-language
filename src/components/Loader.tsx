c3de87b6ad8b1db7507d30f64ef0a19b64124411
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-18 19:53:51 +0500

    Update

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,30 @@
+import { CircularProgress } from "@mui/material";
+
+// const Loader = () => {
+//   return (
+//     <div>
+//       <CircularProgress />
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
+import React from "react";
+
+const centerStyle: React.CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: "100vh", // Ensure the container takes up the full viewport height
+};
+
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div style={centerStyle}>
+      <CircularProgress />
+    </div>
+  );
 };
 
 export default Loader;
